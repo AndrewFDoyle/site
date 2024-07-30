baff8a8c703b2cb71334978496e5abba4bacda5e
AndrewFDoyle <95650439+AndrewFDoyle@users.noreply.github.com>
2024-07-29 22:35:49 -0400

    updated nav bar

--- js/navbar.js
+++ js/navbar.js
@@ -2,11 +2,11 @@ const btn = document.getElementsByClassName('toggle-button')[0]
 const menu = document.getElementsByClassName('navbar-links')[0]
 
 btn.addEventListener('click', () => {
-  menu.classList.toggle('active')
+  menu.classList.toggle('hide')
 })
 
 document.addEventListener('click', e =>{
   if(!menu.contains(e.target) && e.target !== btn) {
-    menu.classList.add('active')
+    menu.classList.add('hide')
   }
 })
